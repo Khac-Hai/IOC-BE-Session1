a3a01f3001c0c51949a013ffdd6b9015f80d723f
Khac-Hai <haikhac23122004@gmail.com>
2026-01-22 09:08:03 +0700

    First commit

---
+++ src/session07/bai1.java
@@ -0,0 +1,18 @@
+package session07;
+
+public class bai1 {
+    public class Student {
+        private String maSinhVien;
+        private String hoTen;
+        private String ngaySinh;
+        private String email;
+        private String soDienThoai;
+
+        public void dangKyMonHoc() {
+        }
+        public void xemDiem() {
+        }
+        public void capNhatThongTin() {
+        }
+    }
+}
